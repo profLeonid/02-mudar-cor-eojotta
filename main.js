f38b565be1b88b2a26cf7e06fe6e8bc54cdeea40
JOÃO PEDRO DOS SANTOS ROMUALDO <25203692@CFP127ALUNO.LOCAL>
2026-02-23 15:20:14 -0300

    finalizado

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const botaoTrocarCor = document.getElementById('botao-trocar-cor')
+const botaoVermelho = document.getElementById('botao-trocar-cor-vermelho')
+const botaoVerde = document.getElementById('botao-trocar-cor-verde')
 
 function trocarCor () {
        let corUsuario = document.getElementById('cor-usuario').value
@@ -19,10 +21,23 @@ function trocarCor () {
             corUsuario = 'yellow'
         }
 
-
       document.documentElement.style.setProperty('--color-bg', corUsuario)
 }
+    function vermelho (){
+        let corUsuario = document.getElementById('cor-usuario').value
+        corUsuario = "red"
+        document.documentElement.style.setProperty('--color-bg', corUsuario)
+
+    }
+
+    function verde (){
+        let corUsuario = document.getElementById('cor-usuario').value
+        corUsuario = "green"
+        document.documentElement.style.setProperty('--color-bg', corUsuario)
+    }
+
 
 botaoTrocarCor.addEventListener('click' , trocarCor)
-
+botaoVermelho.addEventListener('click', vermelho)
+botaoVerde.addEventListener('click', verde)
 
